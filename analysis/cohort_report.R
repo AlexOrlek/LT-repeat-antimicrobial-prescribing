597a337585d0ecbe03f9c74dd4348dc8c03b2686
Alex Orlek <alex.orlek@wolfson.ox.ac.uk>
2022-05-24 16:51:01 +0100

    updated analysis outputs; added demographic pyramid plot

--- analysis/cohort_report.R
+++ analysis/cohort_report.R
@@ -1,17 +1,39 @@
-pacman::p_load('tidyverse')
+pacman::p_load(tidyverse)
 theme_set(theme_bw())
 args = commandArgs(trailingOnly=TRUE)
 cohort_date = args[1]
 
 # read cohort data
 cohort_df <- read.csv(here::here("output", gsub("%s", cohort_date, "input_%s.csv")), header = TRUE)
-cohort_df <- cohort_df %>% mutate(amr_6_months_binary = ifelse(amr_6_months > 0 , 1, 0))
-#cohort_df %>% head()
 
-# proportion LT-repeat
-lt_repeat_prop <- as.data.frame(table(cohort_df$amr_6_months_binary)) %>% rename(LT_repeat = Var1)
-write.csv(lt_repeat_prop, here::here("output", gsub("%s", cohort_date, "lt_repeat_prop_%s.csv")), col.names = TRUE, row.names = FALSE)
+# filter for LT-repeat and count population
+n_total <- nrow(cohort_df)
+cohort_df <- cohort_df %>% filter(amr_6_months > 0)
+n_lt_repeat <- nrow(cohort_df)
+n_lt_repeat_df <- data.frame('n_total' = n_total, 'n_lt_repeat' = n_lt_repeat)
+write.csv(n_lt_repeat_df, here::here("output", gsub("%s", cohort_date, "n_lt_repeat_%s.csv")), col.names = TRUE, row.names = FALSE)
 
-# plot age distribution
-p_age <- ggplot(cohort_df, aes(x = age)) + geom_histogram()
-ggsave(plot = p_age, filename = gsub("%s", cohort_date, "age_distribution_%s.pdf"), path = here::here("output"), device = "pdf")
+# categorise age
+cohort_df <- cohort_df %>% mutate(age_cat = case_when(age >= 18 & age < 30 ~ '18-29',
+                                         age >= 30  & age < 40 ~ '30-39',
+                                         age >= 40  & age < 50 ~ '40-49',
+                                         age >= 50  & age < 60 ~ '50-59',
+                                         age >= 60  & age < 70 ~ '60-69',
+                                         age >= 70  & age < 80 ~ '70-79',
+                                         age >= 80  & age <= 120 ~ '80-120')) %>% mutate(age_cat = as.factor(age_cat))
+
+# exclude missing/other sex
+age_sex_df <- cohort_df %>% filter(sex %in% c('F','M'))
+n_age_sex <- age_sex_df %>% nrow()
+
+# plot demographic pyramid
+# count age-sex and make female counts negative
+age_sex_df <- age_sex_df %>% count(age_cat, sex) %>% complete(age_cat = levels(age_cat), sex = c("F", "M"), fill = list(n = 0))
+age_sex_df <- age_sex_df %>% mutate(n = ifelse(sex == 'F', n * -1, n))
+
+# plot age-sex pyramid
+p_age_sex <- ggplot() + geom_col(data = age_sex_df, mapping = aes(x = age_cat, y = n, fill = sex), colour = "white", width = 1) +   # white around each bar; bar width = 1 means no gap between bars
+  coord_flip() +  # flip the X and Y axes to make pyramid vertical
+  theme_bw() + scale_y_continuous(label = as_labeller(function(x) abs(as.numeric(x)))) +
+  labs(x = "Age group", y = "Number of patients by age/sex", fill = NULL, caption = str_c('Total N = ',n_age_sex))
+ggsave(plot = p_age_sex, filename = gsub("%s", cohort_date, "demographic_pyramid_%s.pdf"), path = here::here("output"), device = "pdf")
