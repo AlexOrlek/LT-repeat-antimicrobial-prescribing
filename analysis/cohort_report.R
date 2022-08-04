616027b4fdd3a658bc476ec49f2657a01660aa12
Alex Orlek <alex.orlek@wolfson.ox.ac.uk>
2022-08-05 00:27:27 +0100

    including non-repeat outputs for clinical conditions analysis

--- analysis/cohort_report.R
+++ analysis/cohort_report.R
@@ -2,6 +2,7 @@ pacman::p_load(tidyverse, data.table)
 theme_set(theme_bw())
 args = commandArgs(trailingOnly=TRUE)
 cohort_date = args[1]
+small_count_threshold <- 5
 
 # read cohort data
 cohort_df <- read.csv(here::here("output", gsub("%s", cohort_date, "input_%s.csv")), header = TRUE)
@@ -15,76 +16,125 @@ write.csv(n_prescribing_df, here::here("output", gsub("%s", cohort_date, "n_pres
 
 
 # ---------------------------
-# COUNT REPEAT PRESCRIBING CLINICAL CONDITIONS
-# clinical conditions (indications and comorbidities) are binary variables ("has_" columns); recode to 0 unless patient has repeat_amr
+# COUNT REPEAT AND NON REPEAT PRESCRIBING CLINICAL CONDITIONS
+# clinical conditions (indications and comorbidities) are binary variables ("has_" columns); recode to 0 unless patient has repeat_amr / non_repeat_amr (append new columns)
 cohort_df <- cohort_df %>%
   mutate(across(
     .cols = starts_with("has_"),
-    .fns = ~ ifelse(repeat_amr == 1, .x, 0)
+    .fns = ~ ifelse(repeat_amr == 1, .x, 0),
+    .names = "repeat_amr_{.col}"
+  )) %>% mutate(across(
+    .cols = starts_with("has_"),
+    .fns = ~ ifelse(non_repeat_amr == 1, .x, 0),
+    .names = "non_repeat_amr_{.col}"
   ))
 
 # append has_any_clinical_condition
-cohort_df <- cohort_df %>% rowwise() %>% mutate(has_any_clinical_condition = sum(c_across(starts_with('has_')))) %>% ungroup() %>% mutate(has_any_clinical_condition = ifelse(has_any_clinical_condition > 0, 1, 0))
+cohort_df <- cohort_df %>% rowwise() %>% mutate(repeat_amr_has_any_clinical_condition = sum(c_across(starts_with('repeat_amr_has_')))) %>% ungroup() %>% mutate(repeat_amr_has_any_clinical_condition = ifelse(repeat_amr_has_any_clinical_condition > 0, 1, 0))
+cohort_df <- cohort_df %>% rowwise() %>% mutate(non_repeat_amr_has_any_clinical_condition = sum(c_across(starts_with('non_repeat_amr_has_')))) %>% ungroup() %>% mutate(non_repeat_amr_has_any_clinical_condition = ifelse(non_repeat_amr_has_any_clinical_condition > 0, 1, 0))
 
-# sum clinical conditions and calculate proportion relative to sum_repeat_amr (i.e. repeat antibiotic prescription patients a given clinical condition assignment, as proportion of total repeat prescription patients)
-n_indications <- cohort_df %>%
+# sum repeat and non_repeat clinical conditions and calculate proportion relative to sum_repeat_amr / sum_non_repeat_amr (i.e. the number of repeat / non-repeat antibiotic prescription patients assigned a given clinical condition, as a count and a proportion of total repeat / non-repeat prescription patients)
+n_indications_repeat_amr <- cohort_df %>%
   summarise(across(
-    .cols = c(repeat_amr, starts_with("has_")),  # summarise across repeat_amr and indications
+    .cols = c(repeat_amr, starts_with("repeat_amr_has_")),  # summarise across repeat_amr and repeat_amr indications
     .fns = sum,
     .names = "sum_{.col}")
-  ) %>% mutate(across(everything(), .fns = ~ ifelse(.x <5, NA, .x))) %>% mutate(across(  # small count suppression
-    .cols = starts_with("sum_has"),  # mutate across summed indications...
+  ) %>% mutate(across(everything(), .fns = ~ ifelse(.x < small_count_threshold, NA, .x))) %>% mutate(across(  # small count suppression
+    .cols = starts_with("sum_repeat_amr_has"),  # mutate across summed indications...
     .fns = ~ .x / sum_repeat_amr,  # ...divide by sum of repeat_amr
     .names = "prop_{.col}")
   )
-write.csv(n_indications, here::here("output", gsub("%s", cohort_date, "n_indications_%s.csv")), row.names = FALSE)
+n_indications_non_repeat_amr <- cohort_df %>%
+  summarise(across(
+    .cols = c(non_repeat_amr, starts_with("non_repeat_amr_has_")),  # summarise across non_repeat_amr and non_repeat_amr indications
+    .fns = sum,
+    .names = "sum_{.col}")
+  ) %>% mutate(across(everything(), .fns = ~ ifelse(.x < small_count_threshold, NA, .x))) %>% mutate(across(  # small count suppression
+    .cols = starts_with("sum_non_repeat_amr_has"),  # mutate across summed indications...
+    .fns = ~ .x / sum_non_repeat_amr,  # ...divide by sum of non_repeat_amr
+    .names = "prop_{.col}")
+  )
+write.csv(n_indications_repeat_amr, here::here("output", gsub("%s", cohort_date, "n_indications_repeat_amr_%s.csv")), row.names = FALSE)
+write.csv(n_indications_non_repeat_amr, here::here("output", gsub("%s", cohort_date, "n_indications_non_repeat_amr_%s.csv")), row.names = FALSE)
 
-# matrix of co-occurrence between clinical conditions
-co_occurrence_matrix <- as.data.frame(crossprod(as.matrix(cohort_df %>% select(starts_with('has_')) %>% select(-has_any_clinical_condition))))
-co_occurrence_matrix <- co_occurrence_matrix %>% mutate(across(everything(), .fns = ~ ifelse(.x <5, NA, .x)))  # small count suppression
-write.table(co_occurrence_matrix, here::here("output", gsub("%s", cohort_date, "n_indications_co_occurrence_%s.csv")), sep = ',', row.names = colnames(co_occurrence_matrix), col.names = NA)
+# matrices of co-occurrence between clinical conditions
+repeat_amr_co_occurrence_matrix <- as.data.frame(crossprod(as.matrix(cohort_df %>% select(starts_with('repeat_amr_has_')) %>% select(-repeat_amr_has_any_clinical_condition))))
+repeat_amr_co_occurrence_matrix <- repeat_amr_co_occurrence_matrix %>% mutate(across(everything(), .fns = ~ ifelse(.x < small_count_threshold, NA, .x)))  # small count suppression
+write.table(repeat_amr_co_occurrence_matrix, here::here("output", gsub("%s", cohort_date, "n_indications_co_occurrence_repeat_amr_%s.csv")), sep = ',', row.names = colnames(repeat_amr_co_occurrence_matrix), col.names = NA)
+non_repeat_amr_co_occurrence_matrix <- as.data.frame(crossprod(as.matrix(cohort_df %>% select(starts_with('non_repeat_amr_has_')) %>% select(-non_repeat_amr_has_any_clinical_condition))))
+non_repeat_amr_co_occurrence_matrix <- non_repeat_amr_co_occurrence_matrix %>% mutate(across(everything(), .fns = ~ ifelse(.x < small_count_threshold, NA, .x)))  # small count suppression
+write.table(non_repeat_amr_co_occurrence_matrix, here::here("output", gsub("%s", cohort_date, "n_indications_co_occurrence_non_repeat_amr_%s.csv")), sep = ',', row.names = colnames(non_repeat_amr_co_occurrence_matrix), col.names = NA)
 
 
 # ---------------------------
-# COUNT REPEAT PRESCRIBING ACROSS CLASSES
-# sum repeat prescribing across antibiotic classes and across clinical conditions
+# COUNT REPEAT AND NON REPEAT PRESCRIBING ACROSS CLASSES AND CLINICAL CONDITIONS
 # note, antibiotic columns ("_6_months") give number of prescriptions in a 6 month lookback period
+
+# repeat
 has_cols <- colnames(cohort_df)[colnames(cohort_df) %>% str_starts('has_')]
 has_cols <- c(NA, has_cols)
 n_repeat_amr_subsets <- list()
 for (has_col in has_cols) {
   if (is.na(has_col)) {
-    # count patients receiving antibiotics by class, across all patients
-    n_repeat_amr_all_patients <- cohort_df %>% select(-amr_6_months) %>% mutate(across(  # remove amr_6_months column since repeat_amr has already been calculated; want to get repeat cols for amr classes; then sum repeat_ cols
+    n_repeat_amr_all_patients <- cohort_df %>% mutate(across(  # select repeat cols for amr classes; then sum repeat cols
       .cols = ends_with("_6_months"),  # mutate across antibiotic class columns
-      .fns = ~ ifelse(.x >=3, 1, 0),  # convert to binary
-      .names = "repeat_{.col}")
+      .fns = ~ ifelse(.x >=3, 1, 0),
+      .names = "{.col}_repeat")
     ) %>% summarise(across(
-      .cols = starts_with("repeat_"),  # sum across repeat_ columns (repeat_amr and repeat_ antibiotic class columns)
+      .cols = ends_with("_6_months_repeat"),  # sum across repeat columns
       .fns = sum,
       .names = "sum_{.col}")
-    ) %>% rename_with(~sub(x = ., pattern = "_6_months", ""))  # remove unecessary suffix from column names
+    ) %>% rename_with(~sub(x = ., pattern = "_6_months_repeat", ""))  # remove unecessary suffix from column names
     n_repeat_amr_subsets[['all patients']] <- n_repeat_amr_all_patients
   } else {
-    # count patients receiving antibiotics by class, across patient subsets (patients with a given clinical condition)
     subset_df <- cohort_df[cohort_df[,has_col] > 0,]
-    n_repeat_amr_subset <- subset_df %>% select(-amr_6_months) %>% mutate(across(  # remove amr_6_months column since repeat_amr has already been calculated; want to get repeat cols for amr classes; then sum repeat_ cols
-      .cols = ends_with("_6_months"),  # mutate across antibiotic class columns
-      .fns = ~ ifelse(.x >=3, 1, 0),  # convert to binary
-      .names = "repeat_{.col}")
+    n_repeat_amr_subset <- subset_df %>% mutate(across( 
+      .cols = ends_with("_6_months"),
+      .fns = ~ ifelse(.x >=3, 1, 0),
+      .names = "{.col}_repeat")
     ) %>% summarise(across(
-      .cols = starts_with("repeat_"),  # sum across repeat_ columns (repeat_amr and repeat_ antibiotic class columns)
+      .cols = ends_with("_6_months_repeat"),
       .fns = sum,
       .names = "sum_{.col}")
-    ) %>% rename_with(~sub(x = ., pattern = "_6_months", ""))  # remove unecessary suffix from column names
+    ) %>% rename_with(~sub(x = ., pattern = "_6_months_repeat", ""))
     n_repeat_amr_subsets[[has_col]] <- n_repeat_amr_subset
   }
 }
-
-# combine counts across clinical condition patient subgroups
 n_repeat_amr_combined <- as.data.frame(rbindlist(n_repeat_amr_subsets, idcol = 'clinical condition'))
-n_repeat_amr_combined <- n_repeat_amr_combined %>% mutate(across(starts_with('sum_'), .fns = ~ ifelse(.x <5, NA, .x)))  # small count suppression
+n_repeat_amr_combined <- n_repeat_amr_combined %>% mutate(across(starts_with('sum_'), .fns = ~ ifelse(.x < small_count_threshold, NA, .x)))  # small count suppression
 write.csv(n_repeat_amr_combined, here::here("output", gsub("%s", cohort_date, "n_repeat_amr_%s.csv")), row.names = FALSE)
+
+# non-repeat
+n_non_repeat_amr_subsets <- list()
+for (has_col in has_cols) {
+  if (is.na(has_col)) {
+    n_non_repeat_amr_all_patients <- cohort_df %>% mutate(across(  # select non repeat cols for amr classes; then sum non repeat cols
+      .cols = ends_with("_6_months"),  # mutate across antibiotic class columns
+      .fns = ~ ifelse(.x < 3 & .x > 0, 1, 0),
+      .names = "{.col}_non_repeat")
+    ) %>% summarise(across(
+      .cols = ends_with("_6_months_non_repeat"),  # sum across non repeat columns
+      .fns = sum,
+      .names = "sum_{.col}")
+    ) %>% rename_with(~sub(x = ., pattern = "_6_months_non_repeat", ""))  # remove unecessary suffix from column names
+    n_non_repeat_amr_subsets[['all patients']] <- n_non_repeat_amr_all_patients
+  } else {
+    subset_df <- cohort_df[cohort_df[,has_col] > 0,]
+    n_non_repeat_amr_subset <- subset_df %>% mutate(across( 
+      .cols = ends_with("_6_months"),
+      .fns = ~ ifelse(.x < 3 & .x > 0, 1, 0),
+      .names = "{.col}_non_repeat")
+    ) %>% summarise(across(
+      .cols = ends_with("_6_months_non_repeat"),
+      .fns = sum,
+      .names = "sum_{.col}")
+    ) %>% rename_with(~sub(x = ., pattern = "_6_months_non_repeat", ""))
+    n_non_repeat_amr_subsets[[has_col]] <- n_non_repeat_amr_subset
+  }
+}
+n_non_repeat_amr_combined <- as.data.frame(rbindlist(n_non_repeat_amr_subsets, idcol = 'clinical condition'))
+n_non_repeat_amr_combined <- n_non_repeat_amr_combined %>% mutate(across(starts_with('sum_'), .fns = ~ ifelse(.x < small_count_threshold, NA, .x)))  # small count suppression
+write.csv(n_non_repeat_amr_combined, here::here("output", gsub("%s", cohort_date, "n_non_repeat_amr_%s.csv")), row.names = FALSE)
 
 
 # ---------------------------
@@ -101,7 +151,7 @@ cohort_df <- cohort_df %>% mutate(age_cat = case_when(age >= 18 & age < 30 ~ '18
 # tabular statistics by repeat and non-repeat amr
 f_tabulate_by <- function(grouped_df) {
   # function to calculate statistics on grouped dataframe; counts of repeat and non_repeat prescribing and incidence proportion per 100,000 population
-  tabular_df <- grouped_df %>% summarise(population = n(), n_repeat_amr = sum(repeat_amr), n_non_repeat_amr = sum(non_repeat_amr), .groups = 'drop') %>% mutate(across(c(population, n_repeat_amr, n_non_repeat_amr), .fns = ~ ifelse(.x <5, NA, .x))) %>% mutate(across(starts_with('n_'), .fns = ~ (.x / population) * 100000, .names = 'incidence_prop_{.col}'))
+  tabular_df <- grouped_df %>% summarise(population = n(), n_repeat_amr = sum(repeat_amr), n_non_repeat_amr = sum(non_repeat_amr), .groups = 'drop') %>% mutate(across(c(population, n_repeat_amr, n_non_repeat_amr), .fns = ~ ifelse(.x < small_count_threshold, NA, .x))) %>% mutate(across(starts_with('n_'), .fns = ~ (.x / population) * 100000, .names = 'incidence_prop_{.col}'))
   return(tabular_df)
 }
 
